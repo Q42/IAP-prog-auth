dca18de3a3cef510909f1edad88261aa8343d003
Roan <roanhageman@gmail.com>
2019-05-16 16:48:06 +0200

    Make simple
    Add gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as request from "request";
-import getAuthorizationHeaders from "./getAuthorizationHeaders";
+import * as jsonwebtoken from "jsonwebtoken";
+
+const openIDTokenURL = "https://www.googleapis.com/oauth2/v4/token";
 
 const doIAPEndpointRequest = async (): Promise<void> => {
   const secrets = {
@@ -7,9 +9,54 @@ const doIAPEndpointRequest = async (): Promise<void> => {
     private_key: "supersecret", // Get this from the key json file you can download when creating a key for a service account
     client_email: "notassecret", // Get this from the same file as above
   };
-  const headers = await getAuthorizationHeaders(secrets);
+
+  const privateKey = secrets.private_key;
+  const clientId = secrets.clientID;
+  const serviceAccountEmail = secrets.client_email;
+
+  // 1: Generate JWT
+  const now = Math.floor(new Date().getTime() / 1000);
+  const jwt = jsonwebtoken.sign({
+    iss: serviceAccountEmail,
+    aud: openIDTokenURL,
+    iat: now,
+    exp: now + 120, // Valid for 2 minutes
+    target_audience: clientId, // This needs to be the client id of the IAP protected resource (find me in the google cloud console)
+  }, privateKey, { algorithm: "RS256" });
+
+  // 2: Get OpenID Token
+  const openIDToken = await getOpenIDToken(jwt);
+
+  // 3: Make the header
+  const headers = {
+    "User-Agent": "c2fIAP",
+    "Authorization": `Bearer ${openIDToken}`
+  }
+
+  // 3: Use the header in our request to the IAP-secured endpoint
   const bananaResponse = await getBanana(headers);
   console.info("Did request to IAP secured endpoint: ", bananaResponse.body);
+}
+
+const getOpenIDToken = async (jwt: string): Promise<string> => {
+  // Wrap the request in a promise so we can do stuff synchronously
+  const response = await new Promise<request.Response>((resolve, reject) => {
+    request.post({ 
+      url: openIDTokenURL, 
+      form: {
+        grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        assertion: jwt,
+      } 
+    }, (error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response);
+      }
+    });
+  });
+  const data = JSON.parse(response.body);
+  return data.id_token;
 }
 
 const getBanana = async (headers: request.Headers): Promise<request.Response> => {
